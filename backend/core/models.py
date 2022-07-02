98aea200244b2e8c926c826ead3d95756a540b08
Claudia Hernandez <claudiahernandez.sistemas@gmail.com>
2022-07-02 20:23:03 -0300

    Adiciona django-localflavors. close #35

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from localflavor.br.br_states import STATE_CHOICES
 
 
 class Address(models.Model):
     endereco = models.CharField('Endereço', max_length=200, blank=True, null=True)  # noqa E501
     bairro = models.CharField('Bairro', max_length=200, blank=True, null=True)  # noqa E501
     cidade = models.CharField('Cidade', max_length=200, blank=True, null=True)  # noqa E501
-    uf = models.CharField('Estado', max_length=2, blank=True, null=True)  # noqa E501
+    uf = models.CharField('UF', max_length=2, choices=STATE_CHOICES, blank=True)  # noqa E501
 
     class Meta:
         abstract = True

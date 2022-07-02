8449dc8231d2bd7d6995fe0e095188c22c232c57
rg3915 <claudiahernandez.sistemas@gmail.com>
2022-07-02 18:22:11 -0300

    Adiciona admin consulta. close #27

---
+++ backend/consulta/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+
+from .models import Consulta, Medicamento, PosConsulta
+
+
+@admin.register(Consulta)
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'dependente',
+        'data_consulta',
+        'hora',
+        'especialidade',
+    )
+    list_display_links = ('dependente',)
+    # search_fields = ('name',)
+    # list_filter = ('type',)
+    # date_hierarchy = 'created'
+
+
+@admin.register(PosConsulta)
+class PosConsultaAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'dependente',
+        'consulta',
+    )
+    list_display_links = ('dependente',)
+    # search_fields = ('name',)
+    # list_filter = ('type',)
+    # date_hierarchy = 'created'
+
+
+@admin.register(Medicamento)
+class MedicamentoAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+    )
+    # list_display_links = ('dependente',)
+    search_fields = ('medicamento_prescrito',)
+    # list_filter = ('type',)
+    # date_hierarchy = 'created'
---
+++ backend/consulta/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+from .models import Consulta, Medicamento, PosConsulta
+
+
+class ConsultaForm(forms.ModelForm):
+
+    class Meta:
+        model = Consulta
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['data_consulta'].widget.attrs.update({'class': 'mask-date'})   # noqa E501
+
+
+class PosConsultaForm(forms.ModelForm):
+
+    class Meta:
+        model = PosConsulta
+        fields = '__all__'
+
+
+class MedicamentoForm(forms.ModelForm):
+
+    class Meta:
+        model = Medicamento
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['data_inicio'].widget.attrs.update({'class': 'mask-date'})
+        self.fields['data_fim'].widget.attrs.update({'class': 'mask-date'})
+        self.fields['hora'].widget.attrs.update({'class': 'mask-hora'})

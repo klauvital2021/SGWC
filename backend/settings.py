e322808d3470454150844ed1344996aced90149f
klauvital2021 <claudiahernandez.sistemas@gmail.com>
2022-07-03 02:13:37 -0300

    Configuração para retornar as imagens na tela.

--- backend/settings.py
+++ backend/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
